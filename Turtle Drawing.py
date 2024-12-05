091777d23f6467d75a7b444a51dc83b1b9e014d8
KMY <115706052+kmy-06@users.noreply.github.com>
2024-12-05 17:47:32 +0800

    Create Turtle Drawing.py

---
+++ Turtle Drawing.py
@@ -0,0 +1,30 @@
+import turtle
+slowpoke = turtle.Turtle()
+slowpoke.shape('turtle')
+
+
+def make_shape(t, sides):
+    angle = 360/sides
+    for i in range(0, sides):
+        t.forward(100)
+        t.right(angle)
+make_shape(slowpoke, 5)
+
+
+slowpoke.pencolor('blue')
+slowpoke.penup()
+slowpoke.setposition(-120, 0)
+slowpoke.pendown()
+slowpoke.circle(50)
+
+
+slowpoke.pencolor('red')
+slowpoke.penup()
+slowpoke.setposition(120, 0)
+slowpoke.pendown()
+for i in range (5):
+    slowpoke.forward(100)
+    slowpoke.right(144)
+
+
+turtle.mainloop()
