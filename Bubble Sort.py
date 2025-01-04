8a121f2cab5f5e22fdff5e643d50a08dfdd19c1a
KMY <115706052+kmy-06@users.noreply.github.com>
2025-01-04 22:36:30 +0800

    Update Bubble Sort.py

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -1,13 +1,14 @@
 # Bubble sort is suitable for 10 floating point values or less than 1000 items; other good sorting include: insertionsort, selectionsort. (counting sort does not work.)
 # Bubble sort has a decreasing range of interest: the algorithm keeps track of the range of interest and restricts that range over time to reduce the number of items considered.
 
+
 def bubble_sort(scores, numbers):
   swapped = True
 
   while swapped:
     swapped = False # assume we cannot find a pair to swap
     for i in range(0, len(scores)-1):
-      if scores[1] < scores[i+1]: # change the sign to >, if you want decresing sorting
+      if scores[i] < scores[i+1]: # change the sign to >, if you want decresing sorting
         temp = scores[i]
         scores[i] = scores[i+1]
         scores[i+1] = temp
@@ -22,6 +23,12 @@ number_of_scores = len(scores)
 solution_numbers = list(range(number_of_scores))
 
 bubble_sort(scores,solution_numbers)
+
+print(scores)
+print(solution_numbers)
+
+# [69, 66, 60, 53, 50, 40]
+# [2, 5, 0, 4, 1, 3]
 
 
 # 1000 integers use: heapsort, quicksort, mergesort, quicksort(best)
